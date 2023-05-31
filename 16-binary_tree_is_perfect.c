7c24165dab0ca1433ae9da1bf33e8776fc4d6119
grayolaRSA <gtndanga@gmail.com>
2023-05-31 21:34:49 +0200

    update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,93 +1,84 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_is_perfect - checks whether tree is perfect
- *@tree: root node
- *Return: int
- */
-
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	if ((binary_tree_leaves(tree->right) == binary_tree_leaves(tree->left)) \
-	     && (binary_tree_balance(tree) == 0))
-	    return (1);
-
-	return (0);
-}
-
-
-/**
- *binary_tree_is_leaf - checks if node is leaf
- *@node: node to be checked
+ * binary_tree_is_leaf - checks if a node is a leaf
  *
- *Return: 1 if node is leaf, otherwise 0
+ * @node: pointer to the node to check
+ * Return: 1 if node is leaf, 0 otherwise
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return (0);
-	if (node->right == NULL && node->left == NULL)
-		return (1);
+	int leaf = 0;
+
+	if (node && !(node->left) && !(node->right))
+		leaf = 1;
+
+	return (leaf);
+}
+
+/**
+ * binary_tree_is_parent_full - checks if a node is parent
+ *
+ * @node: pointer to the node to check
+ * Return: 1 if parent, 0 otherwise.
+ */
+
+int binary_tree_is_parent_full(const binary_tree_t *node)
+{
+	int parent = 0;
+
+	if (node && node->left && node->right)
+		parent = 1;
+
+	return (parent);
+}
+
+/**
+ * height - measures the height of a tree
+ *
+ * @tree: root
+ * Return: height
+ */
+
+int height(const binary_tree_t *tree)
+{
+	int left = 0;
+	int right = 0;
+
+	if (tree == NULL)
+		return (-1);
+
+	left = height(tree->left);
+	right = height(tree->right);
+
+	if (left > right)
+		return (left + 1);
+
+	return (right + 1);
+}
+
+/**
+ * binary_tree_is_perfect - checks if binary tree is perfect
+ *
+ * @tree: root
+ * Return: 1 if perfect, 0 otherwise.
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree && height(tree->left) == height(tree->right))
+	{
+		if (height(tree->left) == -1)
+			return (1);
+
+		if (binary_tree_is_leaf(tree->left) && binary_tree_is_leaf(tree->right))
+			return (1);
+
+		if (binary_tree_is_parent_full(tree))
+			return (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right));
+	}
 
 	return (0);
-
-}
-/**
- *binary_tree_leaves - counts the number of leaves
- *@tree:  root node
- *Return: number of leaves
- */
-
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	if (binary_tree_is_leaf(tree))
-		return (1);
-
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-}
-
-/**
- *binary_tree_balance - measures the balance factor of binary tree
- *@tree: root node
- *Return: balance factor
- */
-
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	int height_left, height_right;
-
-	if (tree == NULL)
-		return (0);
-
-	height_left = binary_tree_height(tree->left);
-	height_right = binary_tree_height(tree->right);
-
-	return (height_left - height_right);
-
-}
-
-/**
- * binary_tree_height - calculates the height of a binary tree
- * @tree: pointer to the root node of the tree
- * Return: height of the tree
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_height, right_height;
-
-	if (tree == NULL)
-		return (0);
-
-	left_height = binary_tree_height(tree->left);
-
-	right_height = binary_tree_height(tree->right);
-
-	return (1 + MAX(left_height, right_height));
 }
